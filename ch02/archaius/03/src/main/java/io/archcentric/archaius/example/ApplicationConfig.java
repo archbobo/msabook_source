c4970b06173d5767241670487bc2a6feff92eecc
ArchCentric <startup.yang@yahoo.com>
2016-04-10 22:17:34 +0800

    add third archaius example

---
+++ ch02/archaius/03/src/main/java/io/archcentric/archaius/example/ApplicationConfig.java
@@ -0,0 +1,31 @@
+package io.archcentric.archaius.example;
+
+import java.io.IOException;
+
+import com.netflix.config.ConfigurationManager;
+import com.netflix.config.DynamicPropertyFactory;
+import com.netflix.config.DynamicStringProperty;
+
+public class ApplicationConfig {
+    static {
+        System.setProperty("archaius.configurationSource.defaultFileName", "customConfig.properties");
+    }
+
+    public ApplicationConfig() {
+        cascadeDefaultConfiguration();
+    }
+
+    private void cascadeDefaultConfiguration() {
+        try {
+            ConfigurationManager.loadCascadedPropertiesFromResources("customConfig");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getStringProperty(String key, String defaultValue) {
+        final DynamicStringProperty property = DynamicPropertyFactory.getInstance().getStringProperty(key,
+            defaultValue);
+        return property.get();
+    }
+}
---
+++ ch02/archaius/03/src/test/java/io/archcentric/archaius/example/ApplicationConfigTest.java
@@ -0,0 +1,31 @@
+package io.archcentric.archaius.example;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class ApplicationConfigTest {
+    private ApplicationConfig appConfig = new ApplicationConfig();
+
+    @Test
+    public void shouldRetrieveThePropertyByKey() {
+        String property = appConfig.getStringProperty("hello.message", "default message");
+
+        assertThat(property, is("Hello Archaius!"));
+    }
+
+    @Test
+    public void shouldRetrieveDefaultValueWhenKeyIsNotPresent() {
+        String property = appConfig.getStringProperty("some.key", "default message");
+
+        assertThat(property, is("default message"));
+    }
+
+    @Test
+    public void shouldReadCascadeConfigurationFiles() {
+        String property = appConfig.getStringProperty("cascade.property", "not found");
+
+        assertThat(property, is("cascade value"));
+    }
+}
