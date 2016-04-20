1ea7982f5795acd78fd23e466c41a17fb33ce02b
ArchCentric <startup.yang@yahoo.com>
2016-04-20 22:52:20 +0800

    add more basic archaius usage sample

--- ch02/archaius/01/src/test/java/io/archcentric/archaius/example/ApplicationConfigTest.java
+++ ch02/archaius/01/src/test/java/io/archcentric/archaius/example/ApplicationConfigTest.java
@@ -1,26 +1,49 @@
 package io.archcentric.archaius.example;
 
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
+
+import java.util.Collections;
 
 import org.junit.Test;
 
-import io.archcentric.archaius.example.ApplicationConfig;
+import com.netflix.config.DynamicLongProperty;
+import com.netflix.config.DynamicPropertyFactory;
+import com.netflix.config.DynamicStringListProperty;
+import com.netflix.config.DynamicStringMapProperty;
+import com.netflix.config.DynamicStringProperty;
 
 public class ApplicationConfigTest {
-    private ApplicationConfig appConfig = new ApplicationConfig();
 
     @Test
-    public void shouldRetrieveThePropertyByKey() {
-        String property = appConfig.getStringProperty("hello.message", "default message");
+    public void testBasicStringProps() {
+    	DynamicStringProperty stringProperty = DynamicPropertyFactory.getInstance().getStringProperty("hello.message", "default message");
 
-        assertThat(property, is("Hello Archaius!"));
+        assertThat(stringProperty.get(), equalTo("Hello Archaius!"));
     }
 
     @Test
-    public void shouldRetrieveDefaultValueWhenKeyIsNotPresent() {
-        String property = appConfig.getStringProperty("some.key", "default message");
+    public void shouldRetrieveDefaultValueWhenKeyIsNotPresent() {        
+    	DynamicStringProperty stringProperty = DynamicPropertyFactory.getInstance().getStringProperty("some.key", "default message");
 
-        assertThat(property, is("default message"));
+        assertThat(stringProperty.get(), is("default message"));
+    }
+    
+    @Test
+    public void testBasicListProps() {
+    	DynamicStringListProperty listProperty = new DynamicStringListProperty("listprop", Collections.emptyList());
+    	assertThat(listProperty.get(), contains("value1", "value2", "value3"));
+    }
+    
+    @Test
+    public void testBasicMapProps() {
+    	DynamicStringMapProperty mapProperty = new DynamicStringMapProperty("mapprop", Collections.emptyMap());
+        assertThat(mapProperty.getMap(), allOf(hasEntry("key1", "value1"), hasEntry("key2", "value2")));
+    }
+    
+    @Test
+    public void testLongProperty() {
+    	DynamicLongProperty longProp = DynamicPropertyFactory.getInstance().getLongProperty("longprop", 1000);
+        assertThat(longProp.get(), equalTo(100L));
     }
 }
