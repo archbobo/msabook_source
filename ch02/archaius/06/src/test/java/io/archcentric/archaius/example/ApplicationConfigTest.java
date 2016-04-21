25c07a38e1daf397d0e110aaa225fae559771099
ArchCentric <startup.yang@yahoo.com>
2016-04-21 22:46:21 +0800

    add url overriding sample

---
+++ ch02/archaius/06/src/test/java/io/archcentric/archaius/example/ApplicationConfigTest.java
@@ -0,0 +1,25 @@
+package io.archcentric.archaius.example;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import com.netflix.config.DynamicLongProperty;
+import com.netflix.config.DynamicPropertyFactory;
+public class ApplicationConfigTest {
+	
+	// passing the system property as a parameter to the VM when you start your application
+	// java ... -Darchaius.configurationSource.additionalUrls=your_config.properties
+	// or set it in the code by uncommenting the following code.
+	static {
+	    System.setProperty("archaius.configurationSource.additionalUrls", 
+	    		"https://raw.githubusercontent.com/archcentric/msabook_source/master/ch02/archaius/config/config.properties");
+	}
+    
+    @Test
+    public void testUrlOverridingProperty() {
+    	DynamicLongProperty waitTimeProp = DynamicPropertyFactory.getInstance().getLongProperty("lock.waitTime", 200);
+        assertThat(waitTimeProp.get(), equalTo(500L));
+    }
+}
