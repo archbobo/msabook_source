6a6832c647c42dc484b82fe1661e4abb4b02b522
ArchCentric <startup.yang@yahoo.com>
2016-04-28 07:36:37 +0800

    format code

--- ch02/archaius/07/src/test/java/io/archcentric/archaius/example/ApplicationConfigTest.java
+++ ch02/archaius/07/src/test/java/io/archcentric/archaius/example/ApplicationConfigTest.java
@@ -9,24 +9,26 @@ import org.junit.Test;
 import com.netflix.config.ConfigurationManager;
 import com.netflix.config.DynamicLongProperty;
 import com.netflix.config.DynamicPropertyFactory;
+
 public class ApplicationConfigTest {
-	
-	// passing the system property as a parameter to the VM when you start your application
+
+	// passing the system property as a parameter to the VM when you start your
+	// application
 	// java ... -Darchaius.deployment.environment=prod
 	// or set it in the code.
 	@Before
-	public void setUp() throws Exception{
+	public void setUp() throws Exception {
 		System.setProperty("archaius.deployment.environment", "prod");
 		System.setProperty("archaius.deployment.applicationId", "app");
-		
+
 		String appId = ConfigurationManager.getDeploymentContext().getApplicationId();
-	    ConfigurationManager.loadCascadedPropertiesFromResources(appId);
+		ConfigurationManager.loadCascadedPropertiesFromResources(appId);
 	}
-	
-    @Test
-    public void testProdEnvironmentOverridingProperty() {
-    	DynamicLongProperty waitTimeProp = DynamicPropertyFactory.getInstance().getLongProperty("lock.waitTime", 100);
-        assertThat(waitTimeProp.get(), equalTo(500L));
-    }
-    
+
+	@Test
+	public void testProdEnvironmentOverridingProperty() {
+		DynamicLongProperty waitTimeProp = DynamicPropertyFactory.getInstance().getLongProperty("lock.waitTime", 100);
+		assertThat(waitTimeProp.get(), equalTo(500L));
+	}
+
 }
